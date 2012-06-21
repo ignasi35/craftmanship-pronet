2d7a69477b2120bc3e39e7b8b79a9ad4985a04dc
Ignasi Marimon-Clos <ignasi35@gmail.com>
2012-06-21 19:44:15 +0200

    Fix test (replace sysout by an actual Assertion)

--- src/test/java/com/scarytom/ParseTest.java
+++ src/test/java/com/scarytom/ParseTest.java
@@ -1,16 +1,29 @@
 package com.scarytom;
 
-import nu.xom.*;
-import org.junit.Test;
-
 import java.io.IOException;
+
+import junit.framework.Assert;
+import nu.xom.ParsingException;
+
+import org.junit.Test;
 
 public class ParseTest {
 
     @Test
     public void canParse() throws IOException, ParsingException {
-        NetworkBuilder networkBuilder = NetworkBuilder.buildNetwork("src/main/resources/ProNet.xml");
-        System.out.println(networkBuilder.build().printout());
+        NetworkBuilder networkBuilder =
+            NetworkBuilder.buildNetwork("src/main/resources/ProNet.xml");
+        String expected =
+            "Programmer\tSkills\tRecommends\n"
+                + "Bill\tPHP, Perl, Ruby\tJason, Jill, Nick, Stu\n"
+                + "Dave\tC#, Java\tJill\n"
+                + "Ed\tC++, Python\tLiz, Rick\n"
+                + "Frank\tC++, Perl, Ruby\tNick\n"
+                + "Jason\tJava, Ruby\tDave, Liz\n"
+                + "Jill\tC++, Ruby\tNick\n" + "Liz\tC#, C++, Java\tBill\n"
+                + "Nick\tC#, Java\tStu\n" + "Rick\tJava, PHP\tEd\n"
+                + "Stu\tC++, Perl\tFrank";
+        Assert.assertEquals(expected, networkBuilder.build().printout());
     }
 
 }
