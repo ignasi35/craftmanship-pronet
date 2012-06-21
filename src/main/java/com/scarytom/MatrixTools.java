607e5fd48b2762e1f2b931d896a92db1686b0998
Ignasi Marimon-Clos <ignasi35@gmail.com>
2012-06-21 23:13:42 +0200

    Add printout for double[][]

--- src/main/java/com/scarytom/MatrixTools.java
+++ src/main/java/com/scarytom/MatrixTools.java
@@ -4,18 +4,30 @@ import java.util.Arrays;
 
 public class MatrixTools {
 
-	public static String printout(final int[][] matrix) {
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < matrix.length; i++) {
-			for (int j = 0; j < matrix[i].length; j++) {
-				sb.append(matrix[i][j]).append(' ');
-			}
-			sb.append('\n');
-		}
-		return sb.toString();
-	}
+    public static String printout(final int[][] matrix) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                sb.append(matrix[i][j]).append(' ');
+            }
+            sb.append('\n');
+        }
+        return sb.toString();
+    }
 
-	public static boolean equals(final int[][] previous, final int[][] current) {
-		return Arrays.deepEquals(previous, current);
-	}
+    public static String printout(final double[][] matrix) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                sb.append(matrix[i][j]).append(' ');
+            }
+            sb.append('\n');
+        }
+        return sb.toString();
+    }
+
+    public static boolean equals(final int[][] previous,
+            final int[][] current) {
+        return Arrays.deepEquals(previous, current);
+    }
 }
